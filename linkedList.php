dd08d8ad80b107faeb269f4170c74a903815ad4e
Amir Khodabande <amirdp8102@gmail.com>
2021-03-17 09:22:52 +0330

    Add functions to Insert  the first node and Print list data

--- linkedList.php
+++ linkedList.php
@@ -23,6 +23,32 @@ class linkedList
         $this->size = 0;
     }
 
+    //    Insert First Node
+    public function insertFirst($data)
+    {
+        $this->head = new Node($data, $this->head);
+        $this->size++;
+    }
+
+    //    Print List Data
+    public function printListData()
+    {
+        $current = $this->head;
+        while ($current) {
+            echo $current->data . "<br>";
+            $current = $current->next;
+        }
+    }
+
 }
+
+$ll = new linkedList();
+
+/* Insert First*/
+$ll->insertFirst(100);
+$ll->insertFirst(200);
+/* End*/
+
+$ll->printListData();
 
 
